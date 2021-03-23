9f60aeacd76cfed99861d314c0617c5393231086
creatorofthesky <26032621+creatorofthesky@users.noreply.github.com>
2021-03-23 01:56:35 -0400

    notes!

--- files/vfio-pci.sh
+++ files/vfio-pci.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # /etc/initramfs-tools/scripts/init-top
+# chmod 755 /etc/initramfs-tools/scripts/init-top/bind_vfio.sh
+# chown root:root /etc/initramfs-tools/scripts/init-top/bind_vfio.sh
+# update-initramfs -u 
 PREREQS=""
 DEVS="0000:41:00.0 0000:41:00.1 0000:41:00.2 0000:41:00.3"
 for DEV in $DEVS;
