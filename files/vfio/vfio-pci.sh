909a97c86f85e23c2823ed968a03fd5aef376351
creatorofthesky <26032621+creatorofthesky@users.noreply.github.com>
2021-03-25 23:24:11 -0400

    isolation!

--- files/vfio/vfio-pci.sh
+++ files/vfio/vfio-pci.sh
@@ -4,7 +4,7 @@
 # chown root:root /etc/initramfs-tools/scripts/init-top/bind_vfio.sh
 # update-initramfs -u 
 PREREQS=""
-DEVS="0000:41:00.0 0000:41:00.1 0000:41:00.2 0000:41:00.3"
+DEVS="0000:0c:00.0 0000:0c:00.1"
 for DEV in $DEVS;
   do echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
 done
