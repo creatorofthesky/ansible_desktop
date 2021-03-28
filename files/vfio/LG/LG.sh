8a78414fb5530bae2092c54ae2fa01b54420b031
creatorofthesky <26032621+creatorofthesky@users.noreply.github.com>
2021-03-28 12:26:27 -0400

    adding a script for lg to launch baseline

---
+++ files/vfio/LG/LG.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+LIBVIRT_DEFAULT_URI='qemu:///system' 
+/usr/bin/virsh start Baseline 
+wait
+/usr/local/bin/looking-glass-client input:rawMouse=true input:autoCapture=true win:minimizeOnFocusLoss=false -F -d"
