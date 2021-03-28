2a19f1450dcdb0d977c40047a98ce48847aeecc0
creatorofthesky <26032621+creatorofthesky@users.noreply.github.com>
2021-03-28 12:51:11 -0400

    owners!

--- files/vfio/LG/LG.sh
+++ files/vfio/LG/LG.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-DISPLAY=:1
-LIBVIRT_DEFAULT_URI='qemu:///system' 
-/usr/bin/virsh start Baseline 
+DISPLAY=:1 LIBVIRT_DEFAULT_URI='qemu:///system' /usr/bin/virsh start Baseline 
 wait
 /usr/local/bin/looking-glass-client input:rawMouse=true input:autoCapture=true win:minimizeOnFocusLoss=false -F -d
