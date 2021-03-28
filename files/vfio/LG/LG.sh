92e166abcdf926fc9dc13821064013726c9793a2
creatorofthesky <26032621+creatorofthesky@users.noreply.github.com>
2021-03-28 12:32:08 -0400

    typos!

--- files/vfio/LG/LG.sh
+++ files/vfio/LG/LG.sh
@@ -2,4 +2,4 @@
 LIBVIRT_DEFAULT_URI='qemu:///system' 
 /usr/bin/virsh start Baseline 
 wait
-/usr/local/bin/looking-glass-client input:rawMouse=true input:autoCapture=true win:minimizeOnFocusLoss=false -F -d"
+/usr/local/bin/looking-glass-client input:rawMouse=true input:autoCapture=true win:minimizeOnFocusLoss=false -F -d
