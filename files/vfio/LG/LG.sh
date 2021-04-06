e3016ea7d81321c291b615a24db4eae94c3d691f
creatorofthesky <26032621+creatorofthesky@users.noreply.github.com>
2021-04-06 13:59:34 -0400

    lg crashing?

--- files/vfio/LG/LG.sh
+++ files/vfio/LG/LG.sh
@@ -3,6 +3,6 @@ cd "${0%/*}"
 #DISPLAY=:1 LIBVIRT_DEFAULT_URI='qemu:///system' 
 nohup virsh start Baseline  &
 wait
-nohup /usr/local/bin/looking-glass-client   win:minimizeOnFocusLoss=false -a -d  &
+nohup /usr/local/bin/looking-glass-client  &
 #input:autoCapture=true
 exec sleep 1
